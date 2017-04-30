b5d8390b1ae5039ca10706994762ec3d96575191
Yu Chen <ychennay@gmail.com>
2017-04-30 10:35:06 -0700

    added configuration and app initializer

---
+++ src/config/AppInitializer.java
@@ -0,0 +1,25 @@
+package config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * Created by ychen4 on 4/30/2017.
+ */
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class[] getRootConfigClasses() {
+        return new Class[] { DiningConfiguration.class };
+    }
+
+    @Override
+    protected Class[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
+}
---
+++ src/config/DiningConfiguration.java
@@ -0,0 +1,14 @@
+package config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * Created by ychen4 on 4/30/2017.
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class DiningConfiguration {
+}
