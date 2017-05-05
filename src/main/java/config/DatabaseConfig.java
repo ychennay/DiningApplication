a13e2dd35c0936fad7910103070808afb4584e91
Yu Chen <ychennay@gmail.com>
2017-05-05 09:54:29 -0700

    attempted dynamodb connection

--- src/main/java/config/DatabaseConfig.java
+++ src/main/java/config/DatabaseConfig.java
@@ -1,26 +1,62 @@
 package main.java.config;
 
+import com.amazonaws.AmazonWebServiceRequest;
+import com.amazonaws.ResponseMetadata;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Region;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClient;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDB;
+import com.amazonaws.services.dynamodbv2.model.*;
+import com.amazonaws.services.dynamodbv2.waiters.AmazonDynamoDBWaiters;
+import org.slf4j.LoggerFactory;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
 
 /**
  * Created by ychen4 on 5/4/2017.
  */
 
-@Configuration
+@EnableAutoConfiguration
 @EnableDynamoDBRepositories
 public class DatabaseConfig {
 
-    @Value("${dynamodb.endpoint}")
-    private String endpoint;
+    private final Logger logger = LoggerFactory.getLogger(DatabaseConfig.getClass());
 
-    @Value("${aws.accesskey}")
-    private String accessKey;
+    @Value("${amazon.dynamodb.endpoint}")
+    private String amazonDynamoDBEndpoint;
 
-    @Value("${aws.secretkey}")
-    private String secretKey;
+    @Value("${amazon.aws.accesskey}")
+    private String amazonAWSAccessKey;
+
+    @Value("${amazon.aws.secretkey}")
+    private String amazonAWSSecretKey;
+
+    @Bean
+    public AmazonDynamoDB amazonDynamoDB(){
+        logger.info("Entering AmazonDynamoDB");
+
+
+         AwsClientBuilder.EndpointConfiguration config = new AwsClientBuilder.EndpointConfiguration(
+                 amazonDynamoDBEndpoint,
+                 "region");
+         AwsClientBuilder.EndpointConfiguration
+
+
+
+        return client;
+    }
 
 
 
---
+++ src/main/java/controller/HelpController.java
@@ -0,0 +1,7 @@
+package main.java.controller;
+
+/**
+ * Created by ychen4 on 5/5/2017.
+ */
+public class HelpController {
+}
