ab75a155839a286227c97896026e53f6efd443e5
Yu Chen <ychennay@gmail.com>
2017-05-06 14:44:20 -0700

    added beginning of security

--- src/main/java/config/DatabaseConfig.java
+++ src/main/java/config/DatabaseConfig.java
@@ -1,49 +1,49 @@
-package main.java.config;
-
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
-import com.amazonaws.util.StringUtils;
-import org.slf4j.LoggerFactory;
-import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import java.util.logging.Logger;
-
-/**
- * Created by ychen4 on 5/4/2017.
- */
-
-@EnableAutoConfiguration
-@EnableDynamoDBRepositories(basePackages = "main.dao")
-public class DatabaseConfig {
-
-    private final Logger logger = (Logger) LoggerFactory.getLogger("Logger");
-
-    @Value("${amazon.dynamodb.endpoint}")
-    private String amazonDynamoDBEndpoint;
-
-    @Value("${amazon.aws.accesskey}")
-    private String amazonAWSAccessKey;
-
-    @Value("${amazon.aws.secretkey}")
-    private String amazonAWSSecretKey;
-
-    @Bean
-    public AmazonDynamoDB amazonDynamoDB(AWSCredentials awsCredentials) {
-        logger.info("Entering AmazonDynamoDB");
-        AmazonDynamoDB amazonDynamoDB = new AmazonDynamoDBClient(awsCredentials);
-        if (StringUtils.isNullOrEmpty(amazonDynamoDBEndpoint)) {
-
-        } else {
-            amazonDynamoDB.setEndpoint(amazonDynamoDBEndpoint);
-        }
-        return amazonDynamoDB;
-    }
-    @Bean
-    public AWSCredentials awsCredentials(){
-        return new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
-    }
-}
+//package main.java.config;
+//
+//import com.amazonaws.auth.AWSCredentials;
+//import com.amazonaws.auth.BasicAWSCredentials;
+//import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+//import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+//import com.amazonaws.util.StringUtils;
+//import org.slf4j.LoggerFactory;
+//import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.context.annotation.Bean;
+//import java.util.logging.Logger;
+//
+///**
+// * Created by ychen4 on 5/4/2017.
+// */
+//
+//@EnableAutoConfiguration
+//@EnableDynamoDBRepositories(basePackages = "main.dao")
+//public class DatabaseConfig {
+//
+//    private final Logger logger = (Logger) LoggerFactory.getLogger("Logger");
+//
+//    @Value("${amazon.dynamodb.endpoint}")
+//    private String amazonDynamoDBEndpoint;
+//
+//    @Value("${amazon.aws.accesskey}")
+//    private String amazonAWSAccessKey;
+//
+//    @Value("${amazon.aws.secretkey}")
+//    private String amazonAWSSecretKey;
+//
+//    @Bean
+//    public AmazonDynamoDB amazonDynamoDB(AWSCredentials awsCredentials) {
+//        logger.info("Entering AmazonDynamoDB");
+//        AmazonDynamoDB amazonDynamoDB = new AmazonDynamoDBClient(awsCredentials);
+//        if (StringUtils.isNullOrEmpty(amazonDynamoDBEndpoint)) {
+//
+//        } else {
+//            amazonDynamoDB.setEndpoint(amazonDynamoDBEndpoint);
+//        }
+//        return amazonDynamoDB;
+//    }
+//    @Bean
+//    public AWSCredentials awsCredentials(){
+//        return new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
+//    }
+//}
---
+++ src/main/java/config/WebSecurityConfiguration.java
@@ -0,0 +1,34 @@
+package main.java.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by ychen4 on 5/6/2017.
+ */
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .antMatchers("/","/home")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+    }
+
+
+}
