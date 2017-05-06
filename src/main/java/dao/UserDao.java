5e90ac96461564e3cdf6392bdaee0e693abc7beb
Yu Chen <ychennay@gmail.com>
2017-05-05 23:34:56 -0700

    begin trying to implement DynamoDB connection

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,7 +1,17 @@
 package main.java.dao;
 
+import main.java.model.User;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
 /**
  * Created by ychen4 on 5/5/2017.
  */
-public class UserDao {
+@EnableScan
+public interface UserDao extends CrudRepository<User, String> {
+
+    List<User> findByFirstName(String firstName);
+
 }
